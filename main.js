82b8f274b10df3d85a0665a16dd363c16b947ada
Tarashrita Ganguly <106442772+Tarashrita@users.noreply.github.com>
2023-06-20 18:24:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+NoseX=0;
+NoseY=0;
+
+function preload(){
+clown_nose= loadImage('https://i.postimg.cc/nrqNcpvM/download-removebg-preview.png');
+}
+
+function setup(){
+    canvas= createCanvas(300, 300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    image(video,0,0,300,300); 
+    image(clown_nose,NoseX,NoseY,50,50);  
+}
+
+function take_snapshot(){
+    save("filterImage.png");
+}
+
+function modelLoaded(){
+    console.log('poseNet model initiated!');
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        NoseX=results[0].pose.nose.x-25;
+        NoseY=results[0].pose.nose.y-1;
+        console.log("results nose x="+NoseX);
+        console.log("results nose y="+NoseY);
+    }
+}
